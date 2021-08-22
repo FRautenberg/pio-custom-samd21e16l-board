237a78aa453d70d6a3b2cef3cbe5e128610adb44
FRautenberg <FRautenberg@users.noreply.github.com>
2021-08-22 21:45:14 +0200

    new platform packages that remove the USB Part

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,16 @@ void setup() {
    * custom_arduino_variant/custom_atsamd21e16l/variant.cpp 
    */
   pinMode(0, OUTPUT); /* D0 = Port A pin 8 (PA8)*/
+  digitalWrite(0, HIGH); /* PA8 high */
+  delay(500);
+  digitalWrite(0, LOW); /* PA8 low */
+  delay(500);
 
   /* can also use direct register accesss (see wiring_digital.c of Adafruit SAMD core) */
-  PORT->Group[PORTA].PINCFG[27].reg=(uint8_t)(PORT_PINCFG_INEN); /* PA 27 support reading back current value (even as output pin) */
-  PORT->Group[PORTA].DIRSET.reg = (1 << 27); /* PA 27 set direction to ouput */
-  PORT->Group[PORTA].OUTCLR.reg = (1 << 27); /* PA 27 to LOW (clear)*/
-  PORT->Group[PORTA].OUTSET.reg = (1 << 27); /* PA 27 to HIGH (set)*/
+  PORT->Group[PORTA].PINCFG[15].reg=(uint8_t)(PORT_PINCFG_INEN); /* PA 15 support reading back current value (even as output pin) */
+  PORT->Group[PORTA].DIRSET.reg = (1 << 15); /* PA 15 set direction to ouput */
+  PORT->Group[PORTA].OUTCLR.reg = (1 << 15); /* PA 15 to LOW (clear)*/
+  PORT->Group[PORTA].OUTSET.reg = (1 << 15); /* PA 15 to HIGH (set)*/
 
   //Serial.begin(115200); /* Start USB-CDC serial driver */
   Serial1.begin(115200); /* start hardware UART (TX = PA06, RX = PA07) */
